c6ddc874c480deb6c7709cba5d7999c6ee7d6d06
Mathilde Ferrand <mathilde.ferrand.coeurderoy@gmail.com>
2020-07-09 09:29:31 +0200

    initial commit

---
+++ WeatherSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeatherSwiftUI
+//
+//  Created by Mathilde Ferrand on 09/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Mathilde Ferrand on 09/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
